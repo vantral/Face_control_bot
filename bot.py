95f0228db16b943fd8cd2ba56d9a9ec914232a7e
Vantral <anton.buzanov.00@gmail.com>
2020-06-05 02:01:51 +0900

    hsd

--- bot.py
+++ bot.py
@@ -160,6 +160,6 @@ def webhook():
 
 if __name__ == '__main__':
     import os
-    app.debug = True
+    app.debug = False
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
