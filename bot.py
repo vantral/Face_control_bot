8b3c1d86923fbe07a0b768ba21ef7915ad9cea9a
Vantral <anton.buzanov.00@gmail.com>
2020-06-05 02:21:09 +0900

    hsd

--- bot.py
+++ bot.py
@@ -41,7 +41,7 @@ def put_text_pil(img, txt):
     w = int((img.shape[1] - w)/2)
     h = img.shape[0] * 0.85
 
-    while h + img.shape[0] >= img.shape[0] * 0.9:
+    while h + img.shape[0] <= img.shape[0] * 0.9:
         h -= img.shape[0] / 100
 
     for off in range(offset):
