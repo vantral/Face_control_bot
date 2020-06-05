6094185bbebca8f041433da22d4e1f3b3c82c25c
Vantral <anton.buzanov.00@gmail.com>
2020-06-05 18:59:06 +0900

    Model

--- bot.py
+++ bot.py
@@ -5,6 +5,7 @@ import cv2
 from PIL import Image, ImageDraw, ImageFont
 import conf  # импортируем наш секретный токен
 import flask
+import random
 
 WEBHOOK_URL_BASE = "https://{}:{}".format(conf.WEBHOOK_HOST, conf.WEBHOOK_PORT)
 WEBHOOK_URL_PATH = "/{}/".format(conf.TOKEN)
@@ -66,6 +67,44 @@ def put_text_pil(img, txt):
     img = np.asarray(im)
     return img
 
+def put_text_face(img, txt, w, h):
+    font_size = img.shape[0] // 20
+
+    im = Image.fromarray(img)
+
+    font = ImageFont.truetype('3952.ttf', size=font_size)
+
+    draw = ImageDraw.Draw(im)
+    wid, hei = draw.textsize(txt, font=font)
+    h += 2 * hei
+    im = Image.fromarray(img)
+    d = ImageDraw.Draw(im)
+
+    offset = font_size // 25
+    shadowColor = 'black'
+
+    for off in range(offset):
+        # move right
+        d.text((w-off, h), txt, font=font, fill=shadowColor)
+        # move left
+        d.text((w+off, h), txt, font=font, fill=shadowColor)
+        # move up
+        d.text((w, h+off), txt, font=font, fill=shadowColor)
+        # move down
+        d.text((w, h-off), txt, font=font, fill=shadowColor)
+        # diagnal left up
+        d.text((w-off, h+off), txt, font=font, fill=shadowColor)
+        # diagnal right up
+        d.text((w+off, h+off), txt, font=font, fill=shadowColor)
+        # diagnal left down
+        d.text((w-off, h-off), txt, font=font, fill=shadowColor)
+        # diagnal right down
+        d.text((w+off, h-off), txt, font=font, fill=shadowColor)
+
+    # теперь можно центрировать текст
+    d.text((w, h), txt, fill='rgb(255, 255, 255)', font=font)
+    img = np.asarray(im)
+    return img
 
 def main_menu(name):
     mark_up = telebot.types.InlineKeyboardMarkup()
@@ -103,6 +142,46 @@ def gotit(message):
         mark_up.add(item)
         bot.send_message(id, text=u'Выбери режим!', reply_markup=mark_up)
     if message.data == '2':
+        SRC = ['']
+        FILE_INFO = ['']
+        bot.send_message(id, text=u'Отправь мне фото!')
+
+        @bot.message_handler(content_types=['photo'])
+        def face_control(message):
+            FILE_INFO[0] = bot.get_file(message.photo[-1].file_id)
+            SRC[0] = message.chat.id
+            if FILE_INFO[0] and SRC[0] == message.chat.id:
+                downloaded_file = bot.download_file(FILE_INFO[0].file_path)
+                with open(str(SRC[0]), 'wb') as new_file:
+                    new_file.write(downloaded_file)
+            prototxt_path = os.path.join('model_data/deploy.prototxt')
+            caffemodel_path = os.path.join('model_data/weights.caffemodel')
+            model = cv2.dnn.readNetFromCaffe(prototxt_path, caffemodel_path)
+            img = cv2.imread(str(SRC[0]))
+
+            (h, w) = img.shape[:2]
+            blob = cv2.dnn.blobFromImage(cv2.resize(img, (300, 300)), 1.0, (300, 300), (104.0, 177.0, 123.0))
+
+            model.setInput(blob)
+            detections = model.forward()
+            confidence = 0
+
+            while confidence <= 0.5:
+                i = random.choice(range(0, detections.shape[2]))
+                box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+                (startX, startY, endX, endY) = box.astype("int")
+                confidence = detections[0, 0, i, 2]
+
+            cv2.rectangle(img, (startX, startY), (endX, endY), (0, 0, 0), -1)
+            img = put_text_face(img, 'чорт', startX, startY)
+            cv2.imwrite(str(SRC[0]) + '.png', img)
+            photo = open(str(SRC[0]) + '.png', 'rb')
+            os.remove(str(SRC[0]))
+            os.remove(str(SRC[0]) + '.png')
+            SRC[0] = ''
+            FILE_INFO[0] = ''
+            bot.send_photo(message.chat.id, photo=photo)
+    if message.data == '3':
         SRC = ['']
         FILE_INFO = ['']
         bot.send_message(id, text=u'Отправь мне фото!')
