269ddbeefcc1307dfb42a1e26fa14b3de1fec8ea
Vantral <42929116+Vantral@users.noreply.github.com>
2021-04-03 15:19:50 +0300

    Update conf.py

--- conf.py
+++ conf.py
@@ -2,6 +2,6 @@ PROXY = {
     'https': 'socks5://321854992:aUg1eb8d@orbtl.s5.opennetwork.cc:999',
     'socks5': 'socks5://321854992:aUg1eb8d@orbtl.s5.opennetwork.cc:999'
 }
-TOKEN = '1295188125:AAH2meA3_Hu00K-ggzPBAPoiuC5MtMeIAAc'
+TOKEN = 'TOKEN'
 WEBHOOK_HOST = 'mighty-fjord-70525.herokuapp.com'
 WEBHOOK_PORT = '443'
